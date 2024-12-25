d686ef33a68cb5ce915794b83f28b36e5dc2fdba
KANISHKSHUKLA <kanishkshukla211@gmail.com>
2024-12-26 03:06:57 +0530

    embeddings

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import huggingface_hub
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
@@ -8,6 +9,9 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from htmlTemp import css, bot_template, user_template
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_google_vertexai import VertexAIEmbeddings
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -18,7 +22,7 @@ def get_pdf_text(pdf_docs):
     return text
 
 
-def get_text_chunks(raw_text):
+def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator= "\n",
         chunk_size = 1000,
@@ -28,36 +32,58 @@ def get_text_chunks(raw_text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-# def get_vectorstore(text_chunks): ############ openai embeddings 
-#     embeddings = OpenAIEmbeddings()
-#     vectorstore = FAISS.from_text(text=text_chunks, embedding = embeddings)
-#     return vectorstore
-
-
-def get_vectorstore(text_chunks): # instructor embeddings 
-    embeddings = HuggingFaceInstructEmbeddings(model_name= "hkunlp/instructor-xl")
-    vectorstore = FAISS.from_text(text=text_chunks, embedding = embeddings)
-    return vectorstore 
+def get_vectorstore(text_chunks):  
+    # embeddings = OpenAIEmbeddings()      ############ openai embeddings
+    embeddings = VertexAIEmbeddings(model="text-embedding-004")
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key= 'chat_history' return_messages=True)
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm = llm,
-        retriever= vectorstore.as_retriever(),
-        memory= memory
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
     )
     return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 def main():
     load_dotenv() 
     st.set_page_config(page_title="Chat with your PDFs")
 
-if "conversation" not in st.session_state:
-    st.session_state.conversation = none         ## when the app is going to re reun it will check whether it is in the convers  and is going to reinitialsise it to none and if it is already initialised it is not gouoing to to do anything
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None         ## when the app is going to re reun it will check whether it is in the convers  and is going to reinitialsise it to none and if it is already initialised it is not gouoing to to do anything
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
     st.header("Chat with your PDF")
-    st.text_input("Ask anything to your pdf")
+    user_question = st.text_input("Ask anything to your pdf")
+    if user_question:
+        handle_user_input(user_question)
+
+
+    st.write(user_template.replace("{{MSG}}", "Hi Bot!"),unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "Hi!"),unsafe_allow_html=True)
+
+    
 
     with st.sidebar:
         st.subheader("Uploaded files")
@@ -70,20 +96,13 @@ if "conversation" not in st.session_state:
                 
 
                 # chunkifiying the text
-                text_chunks = get_text_chunk(raw_text)
+                text_chunks = get_text_chunks(raw_text)
 
                 # vector store 
                 vectorstore = get_vectorstore(text_chunks)
 
                 #convo shain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
-
-
-
-
-
-
-
 
 
 
